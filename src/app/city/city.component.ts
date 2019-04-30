b8b08ed15718b847bd4b9d1b99fab30ded492ba9
Marc <marc.webdev@yahoo.com>
2019-04-30 14:37:34 +0200

    Aanpassingen layout

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,29 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
-import { AppComponent } from '../app.component'; 
-import { WeatherApi } from '../services/weatherApi';
-import { CommunicatorService } from '../services/communicator.service';
-import { WeatherCurrent, WeatherForecast } from '../interfaces/weather';
+import { AppComponent } from "../app.component";
+import { WeatherApi } from "../services/weatherApi";
+import { CommunicatorService } from "../services/communicator.service";
+import { WeatherCurrent, WeatherForecast } from "../interfaces/weather";
 
 @Component({
-  selector: 'app-city',
-  templateUrl: './city.component.html',
-  styleUrls: ['./city.component.scss']
+  selector: "app-city",
+  templateUrl: "./city.component.html",
+  styleUrls: ["./city.component.scss"]
 })
 export class CityComponent {
   @Input() cityName: string;
   @Input() cityCode: string;
   imgPath: string;
   isSelected: boolean = false;
-  
+
   /**
    * Do setup work of component
    * @Constructor
    * @param {AppComponent} appComponent - reference to parent component
    * @param {WeatherApi} weatherService - reference to service that retrieves OWM data
    * @param {CommunicatorService} communicatorService - reference to servivce that allows components to send and receive data
-   */ 
-  constructor(private appComponent: AppComponent, private weatherService: WeatherApi, private communicatorService: CommunicatorService) {
+   */
+
+  constructor(
+    private appComponent: AppComponent,
+    private weatherService: WeatherApi,
+    private communicatorService: CommunicatorService
+  ) {
     this.imgPath = appComponent.imgPath;
   }
 
@@ -32,15 +37,16 @@ export class CityComponent {
    * @getCurrentWeather
    * @return void
    **/
-  getCurrentWeather () {
-    this.weatherService.getCurrentWeather(this.cityCode)
-    .subscribe((data: WeatherCurrent) => {
-      // broadcast response data using the communicatorService
-      this.communicatorService.sendCurrentWeather({
-        temperatureCurrent: data['main'].temp,
-        windSpeedCurrent: data['wind'].speed
+  getCurrentWeather() {
+    this.weatherService
+      .getCurrentWeather(this.cityCode)
+      .subscribe((data: WeatherCurrent) => {
+        // broadcast response data using the communicatorService
+        this.communicatorService.sendCurrentWeather({
+          temperatureCurrent: Math.round(data["main"].temp),
+          windSpeedCurrent: Math.round(data["wind"].speed)
+        });
       });
-    });
   }
 
   /**
@@ -48,14 +54,15 @@ export class CityComponent {
    * @getForecastWeather
    * @return void
    **/
-  getForecastWeather () {
-    this.weatherService.getForecastWeather(this.cityCode)
+  getForecastWeather() {
+    this.weatherService
+      .getForecastWeather(this.cityCode)
       // broadcast response data using the communicatorService
       .subscribe((data: WeatherForecast) => {
-      this.communicatorService.sendForecastWeather({
-        temperatureForecast: data['list'][0].main.temp,
-        windSpeedForecast: data['list'][0].wind.speed
+        this.communicatorService.sendForecastWeather({
+          temperatureForecast: Math.round(data["list"][0].main.temp),
+          windSpeedForecast: Math.round(data["list"][0].wind.speed)
+        });
       });
-    });
   }
 }
